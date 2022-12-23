37b06c62fcfcbaa602574c1e43f6b0c9b22142d9
Gun O Byun <60571418+guno517@users.noreply.github.com>
2022-12-23 18:22:09 +0900

    Time: 231 ms (40.71%), Space: 47.4 MB (53.88%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let sum = 0;
+    const romanMap = {
+        'I' : 1,
+        'V' : 5,
+        'X' : 10,
+        'L' : 50,
+        'C' : 100,
+        'D' : 500,
+        'M' : 1000,
+    }
+    for(let i = 0; i < s.length; i++){
+        const curr = romanMap[s[i]]
+        const next = romanMap[s[i+1]]
+        if(next && curr < next ) {
+            sum += next - curr;
+            i++;
+        }
+		else {
+            sum += curr;
+        }
+    }
+    return sum;
+};
