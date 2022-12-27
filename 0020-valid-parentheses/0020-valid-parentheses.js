3ea4d41142df71eda94110c81c764c08ea024adf
Gun O Byun <60571418+guno517@users.noreply.github.com>
2022-12-27 22:22:39 +0900

    Time: 131 ms (10.98%), Space: 47.7 MB (7.52%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const charArr = [ '(', ')', '{', '}', '[', ']'];
+    const answer = [];
+    if(s.length % 2 !== 0) return false
+    for(let i = 0; i < s.length; i++){
+        if(charArr.indexOf(s[0]) % 2 !== 0) return false
+        if(charArr.indexOf(s[i]) % 2 === 0){
+            answer.push(s[i]);
+            continue;
+        }
+        if(charArr.indexOf(s[i]) - 1 === charArr.indexOf(answer[answer.length - 1]) ){
+            console.log('1',s[i], charArr.indexOf(s[i]) - 1)
+            console.log('2',answer[answer.length - 1], charArr.indexOf(answer[answer.length - 1]))
+            answer.pop();
+        } else {
+            return false
+        }
+    }
+    return answer.length === 0 ? true : false
+};
