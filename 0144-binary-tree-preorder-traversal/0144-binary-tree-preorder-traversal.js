ec49f0a5752b01201aa8e3c2925218232dae10cc
Gun O Byun <60571418+guno517@users.noreply.github.com>
2023-02-02 15:14:04 +0900

    Time: 61 ms (76.97%), Space: 42.1 MB (49.36%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    let answer = []
+    function recursion(node){
+        if(node === null) return;
+        answer.push(node.val)
+        if(node.left){
+            recursion(node.left)
+        }
+        if(node.right){
+            recursion(node.right)
+        }
+    }
+    recursion(root)
+    return answer;
+};
